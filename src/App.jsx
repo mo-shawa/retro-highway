c904abb2f5f3931d96df02cfec259097d5adcf72
moshawa <moshawa0@gmail.com>
2023-02-27 17:29:42 -0500

    first render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from "react"
+import { OrbitControls, useGLTF } from "@react-three/drei"
 import "./App.css"
 
 function App() {
-	return <div className="App"></div>
+	const { scene, nodes } = useGLTF("./gltf/retro-highway_v4_decimated.gltf")
+	console.log(nodes, scene)
+
+	return (
+		<>
+			<color args={["#333333"]} attach="background" />
+			<OrbitControls makeDefault />
+			<primitive object={scene} />
+			<mesh scale={1.5}>
+				<boxGeometry />
+				<meshNormalMaterial />
+			</mesh>
+		</>
+	)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import { Canvas } from "@react-three/fiber"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Canvas
+		camera={{
+			fov: 45,
+			near: 0.1,
+			far: 200,
+			position: [1, 2, 6],
+		}}
+	>
+		<App />
+	</Canvas>
 )
